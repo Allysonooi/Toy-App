3753610179a5d519e1b88a1b7902a635ca7b9d24
Allyson Ooi <allysonooi28@gmail.com>
2016-04-06 19:51:51 +0800

    The End of Toyapp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_many :microposts
-  validates FILL_IN, presence: true
-  validates FILL_IN, presence: true
+  validates :name , presence: true
+  validates :email , presence: true
+
+
+  def details
+    "#{name} and #{email}"
+  end
 end
